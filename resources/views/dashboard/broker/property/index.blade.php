ea73384576efc4c9cdd789d747725f3fbf4f5814
HappyAgarwal918 <happyagarwal918@gmail.com>
2023-09-04 09:36:51 +0530

    property img

--- resources/views/dashboard/broker/property/index.blade.php
+++ resources/views/dashboard/broker/property/index.blade.php
@@ -54,7 +54,7 @@
                       </a>
                     </td> -->
                     <td class="white-sp-normal">{{ $key +1 }}</td>
-                    <td class="white-sp-normal"><img src="{{ asset($properties['propertyimg']['0']->img_src) }}"></td>
+                    <td class="white-sp-normal"><img src="{{ asset($properties['propertyimg']->img_src) }}"></td>
                     <td class="white-sp-normal">{{ $properties->full_address }}</td>
                     <td class="white-sp-normal">{{ $properties->room_type }}</td>
                     <td class="white-sp-normal">{{ $properties->meal_type }}</td>
